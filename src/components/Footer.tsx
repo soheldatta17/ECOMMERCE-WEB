14c597ae6c8cc917580341c21d2ef294bacf3f72
soheldatta17 <soheldatta17@gmail.com>
2024-07-14 19:25:54 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Footer: FC = () => (
+  <div className="bg-blue-500 text-white p-4 text-center mt-auto">
+    <div>&copy; Copyright | All Rights Reserved</div>
+  </div>
+);
+
+export default Footer;
